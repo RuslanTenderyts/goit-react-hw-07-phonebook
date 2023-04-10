cf7723dd63535c0ae3d04ce97e636bb1c4d67c66
RuslanTenderyts <rylya1995@gmail.com>
2023-04-10 21:04:11 +0300

    hw-07-React- createAsyncThunk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchContacts } from "redux/operations";
+import { getError, getIsLoading } from "redux/selectors";
+import { Layout } from "components/Layout/Layout";
+import { GlobalStyle } from "./GlobalStyle"
+import { ContactFormik } from "./ContactForm/ContactFormik";
+import ContactList  from "./ContactList/ContactList" ;
+import Filter from "./Filter/Filter"
+
+
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      <Layout>
+        <h1> PhoneBook </h1>
+        <ContactFormik />
+        <h2> Contacts </h2>
+        <Filter  />
+        {isLoading && !error && <b>Request in progress...</b>}
+        <ContactList />
+        <GlobalStyle/>
+      </Layout>
   );
 };
